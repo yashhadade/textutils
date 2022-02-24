1f1e7ac5481810bd400b439f543b8dbb57267073
prerana1821 <prerananawar1@gmail.com>
2022-02-25 00:36:35 +0530

    fix: router error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Alert from './components/Alert';
 import About from './components/About';
 import {
@@ -13,57 +13,49 @@ import {
 
 //import About from './components/About';
 function App() {
-  const[darkMode,setDarkMode] = useState('light');//whether dark mode is enabled or not 
+  const [darkMode, setDarkMode] = useState('light');//whether dark mode is enabled or not 
   const [alert, setAlert] = useState(null);
-  const showAlert=(message,type)=>{
+  const showAlert = (message, type) => {
     setAlert({
-     msg: message,
-     type:type
+      msg: message,
+      type: type
     })
-    setTimeout(()=>{
+    setTimeout(() => {
       setAlert(null);
 
-    },1500
+    }, 1500
 
     );
   }
-   const toggleMode=()=>{
-    if(darkMode ==='light'){
+  const toggleMode = () => {
+    if (darkMode === 'light') {
 
       setDarkMode('dark');
-      document.body.style.backgroundColor='balck';
-      showAlert("dark mode on ",'sucess');
-      document.title='TextUtils - Dark Mode';
+      document.body.style.backgroundColor = 'balck';
+      showAlert("dark mode on ", 'sucess');
+      document.title = 'TextUtils - Dark Mode';
     }
-    else{
+    else {
 
       setDarkMode('light');
-      document.body.style.backgroundColor='white';
-      showAlert("dark mode on ",'sucess');
-      document.title='TextUtils - light mode';
+      document.body.style.backgroundColor = 'white';
+      showAlert("dark mode on ", 'sucess');
+      document.title = 'TextUtils - light mode';
     }
   }
   return (
-   <>
-  <Routes>
-   <Navbar title="Textutils" moded={darkMode} toggleMode={toggleMode}aboutText="About Textutils"/>
-   <Alert alert={alert}/>
-    <div className="container my-3">
-    
-       
-          <Route path="/about" element={<About/>}>
-            {/* <About /> */}
+    <>
+      <Navbar title="Textutils" moded={ darkMode } toggleMode={ toggleMode } aboutText="About Textutils" />
+      <Alert alert={ alert } />
+      <div className="container my-3">
+        <Routes>
+          <Route path="/about" element={ <About /> }>
           </Route>
-          
-          <Route path="/" element={<Textform showAlert={showAlert} heading="Enter the text to analyze below"mode={darkMode}/>}>
-          {/* {<Textform showAlert={showAlert} heading="Enter the text to analyze below"mode={darkMode}/>} */}
+          <Route path="/" element={ <Textform showAlert={ showAlert } heading="Enter the text to analyze below" mode={ darkMode } /> }>
           </Route>
-       
-       {/*<About/>*/}
-   
-    </div>
- </Routes>
-   </>
+        </Routes>
+      </div>
+    </>
   );
 }
 export default App;
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,54 +1,55 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 export default function Textform(props) {
-    const handleUpClick =()=>{
+    const handleUpClick = () => {
         //console.log("Uppercase was clicked" +text);
-        let newText =text.toUpperCase();
+        let newText = text.toUpperCase();
         setText(newText);
-        props.showAlert('converted to uppercase','sucess')
+        props.showAlert('converted to uppercase', 'sucess')
     }
-    const handleClClick =()=>{
+    const handleClClick = () => {
         //console.log("Uppercase was clicked" +text);
-        let newText ='';
+        let newText = '';
         setText(newText);
-        props.showAlert('text is been clear','sucess')
+        props.showAlert('text is been clear', 'sucess')
     }
-    
-        const handleLoClick =()=>{
-            //console.log("Uppercase was clicked" +text);
-            let newText =text.toLowerCase();
-            setText(newText);
-            props.showAlert('converted to lowecase','sucess')
+
+    const handleLoClick = () => {
+        //console.log("Uppercase was clicked" +text);
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert('converted to lowecase', 'sucess')
     }
-    const handleOnChange =()=>{
+    const handleOnChange = (Event) => {
         //console.log("On change");
+        console.log(Event.target.value);
         setText(Event.target.value)
     }
     const [text, setText] = useState('enter something there');
     //setText("new text");
-  return (
-    <>
-    <div className='container'>
-        <h1>{props.heading} </h1>
-      <div className="mb-3">
-         <textarea className="form-control" value={text}  onChange={handleOnChange} id="myBox" rows="8"></textarea>
-     </div>
-     <button className="btn btn-primary mx-2 my-1"onClick={handleUpClick}>Convert to Upper case</button>
-     <button className="btn btn-primary mx-2 my1"onClick={handleLoClick}>Convert to Lower case</button>
-     <button className="btn btn-primary mx-2 my1"onClick={handleClClick}>Clear text</button>
-    </div>
-    <div className="container my-3">
-        <h2>
-            Your text summery
-        </h2>
-        <p>
-            {text.split(" ").filter((element)=>{return element.lenght!=0}).length} words,{text.length} charecters
-        
-        </p>
-        <p>{0.008*text.split(" ").length} Minutes read</p>
-        <h2>Preview</h2>
-        <p>{text}</p>
-    </div>
-    </>
-  )
-  }
+    return (
+        <>
+            <div className='container'>
+                <h1>{ props.heading } </h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={ text } onChange={ handleOnChange } id="myBox" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-2 my-1" onClick={ handleUpClick }>Convert to Upper case</button>
+                <button className="btn btn-primary mx-2 my1" onClick={ handleLoClick }>Convert to Lower case</button>
+                <button className="btn btn-primary mx-2 my1" onClick={ handleClClick }>Clear text</button>
+            </div>
+            <div className="container my-3">
+                <h2>
+                    Your text summery
+                </h2>
+                <p>
+                    { text.split(" ").filter((element) => { return element.lenght != 0 }).length } words,{ text.length } charecters
+
+                </p>
+                <p>{ 0.008 * text.split(" ").length } Minutes read</p>
+                <h2>Preview</h2>
+                <p>{ text }</p>
+            </div>
+        </>
+    )
+}
